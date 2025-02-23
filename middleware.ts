ed78709c26756fd28d62067117c14c2b676ddc2a
Rohan Nayan <rohannayan405@gmail.com>
2025-02-23 20:25:18 +0530

    Configured Clerk

---
+++ middleware.ts
@@ -0,0 +1,42 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+
+const isPublicRoute = createRouteMatcher([
+    '/signin',
+    '/signup',
+    '/',
+    '/home'
+]);
+
+const isPublicApiRoute = createRouteMatcher([
+    '/api/videos'
+])
+
+
+export default clerkMiddleware(async (auth, req) => {
+    const { userId } = await auth();
+    const currentUrl = new URL(req.url);
+
+    const isAccessingHome = currentUrl.pathname === '/home';
+    const isApiRequest = currentUrl.pathname.startsWith('/api');
+
+    if (userId && isPublicRoute(req) && !isAccessingHome) {
+        return NextResponse.redirect(new URL('/home', req.url));
+    }
+    if(!userId){
+        //if user not logged in and trying to access a protected route
+        if(!isPublicApiRoute(req) && !isPublicRoute(req)){
+            return NextResponse.redirect(new URL('/signin', req.url));
+        }
+
+        //if the request is for a protected API and the user is not logged in
+        if(isApiRequest && !isPublicApiRoute(req)){
+            return NextResponse.redirect(new URL('/signin', req.url));
+        }
+    }
+    return NextResponse.next();
+});
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
